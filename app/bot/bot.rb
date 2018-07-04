477657d97e93ec34acfc5874011f093dbfbda026
CharlyMorgan <charlymorgan0@gmail.com>
2018-07-04 14:54:49 +0100

    env token charged

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messanger'
 
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
