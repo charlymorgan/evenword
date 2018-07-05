d5dc55c3d0b2eb2df42b43f997ddb432f12d44f2
CharlyMorgan <charlymorgan0@gmail.com>
2018-07-05 18:43:38 +0100

    add new structure

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,8 @@
 require 'facebook/messanger'
 
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
+
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
